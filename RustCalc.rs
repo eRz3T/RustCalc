e207fea6ce93a5bffdac9b9950076b638099197e
eRz3T <bartek.c12@gmail.com>
2023-05-20 03:27:08 +0200

    Adding two numbers - interactive

--- RustCalc.rs
+++ RustCalc.rs
@@ -1,6 +1,44 @@
-fn main(){
-let a = 5;
-let b = 5;
+use std::io;
 
-println!("A={a} plus B={b} is equal to {}", a+b);
+fn main() {
+
+
+// Create a mutable string variable to store user input
+    let mut input = String::new(); 
+  
+
+println!("Adding two numbers");
+    // Prompt the user to enter the value for x
+        println!("Enter the value for x:"); 
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Read error"); 
+    // Read the input from the user and handle any potential errors
+
+
+
+
+// Parse the input string to an i32 value, trimming any whitespace and handling parsing errors
+    let x: i32 = input.trim().parse().expect("Invalid number"); 
+    input.clear(); 
+// Clear the input string to reuse it for the next input
+
+
+
+    // Prompt the user to enter the value for y
+        println!("Enter the value for y:"); 
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Read error"); 
+    // Read the input from the user and handle any potential errors
+
+
+
+// Parse the input string to an i32 value, trimming any whitespace and handling parsing errors
+    let y: i32 = input.trim().parse().expect("Invalid number"); 
+
+    println!("x = {}, y = {}", x, y);
+    println!("Sum = {}", x + y );
+// Print the values of x, y and sum 
+
 }
